ad2e38e08d0fbe7c8f2bf1ba2ecb9a3ef1694f23
opaDam <54212616+opaDam@users.noreply.github.com>
2021-12-15 11:49:42 +0100

    01

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const audio_ = document.querySelector('#audio').autoplay = true;
 
-if (audio_ !== undefined) {
-  promise.then(_ => {
-    // Autoplay started!
-  }).catch(error => {
-    // Autoplay was prevented.
-    // Show a "Play" button so that user can start playback.
-  });
-}
-
 let play_ = () => {
     console.log(audio_)
     audio_.play()
