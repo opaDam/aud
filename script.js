a98ed15688d813c1e2e5c42177a5f0ea587830f7
opadam <54212616+opaDam@users.noreply.github.com>
2021-12-31 19:10:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
   function my(){
-    // audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/s.mp3")
-//     audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view")
+
+//  audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view")
     audioPlayer = new Audio("./data/a1.mp3") //  localhost
     audioPlayer.play()
     document.querySelector(".btn_play").style="display: none"
     }
 
-  // * "./data/a1.mp3"
 
- // https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view
+
+
