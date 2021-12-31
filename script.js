1155ad880f986efcdf6e486a413a145ddda9075d
opaDam <54212616+opaDam@users.noreply.github.com>
2021-12-31 18:27:46 +0100

    ok

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
   function my(){
-    audioPlayer = new Audio("./data/s.mp3")
+    audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/s.mp3")
     audioPlayer.play()
     document.querySelector(".btn_play").style="display: none"
     }
 
+    // * http://127.0.0.1:5500/audio/index.html
+    // * https://drive.google.com/drive/u/0/my-drive/s(1).mp3
+    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing/s(1).mp3
+    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/s(1).mp3
+    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
+    // * "./data/s.mp3"
+
+    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
