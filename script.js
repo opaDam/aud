c789b46c167a48ebb6b8932d8fb75d3f79a82f4b
opaDam <54212616+opaDam@users.noreply.github.com>
2021-12-31 18:42:16 +0100

    ok

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 
   function my(){
     audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/s.mp3")
+    // audioPlayer = new Audio(URL="./data/a1.mp3") //  localhost
     audioPlayer.play()
     document.querySelector(".btn_play").style="display: none"
     }
 
-    // * http://127.0.0.1:5500/audio/index.html
-    // * https://drive.google.com/drive/u/0/my-drive/s(1).mp3
-    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing/s(1).mp3
-    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/s(1).mp3
-    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
-    // * "./data/s.mp3"
+  // * "./data/a1.mp3"
 
-    // * https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
+ 
