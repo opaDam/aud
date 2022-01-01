fb89d502f4da300d65d9fb63214f7f1faf90d021
opadam <54212616+opaDam@users.noreply.github.com>
2022-01-01 11:47:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     audioPlayer = new Audio(song)
 //     audioPlayer = new Audio("./data/a1.mp3") //  localhost
     audioPlayer.play()
-    document.querySelector(".btn_play").style="display: block"
+    document.querySelector(".btn_play").style="display: flex"
     }
 
 // https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
