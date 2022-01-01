2018d681b5263eace5083a62a4445e34016025e5
opadam <54212616+opaDam@users.noreply.github.com>
2022-01-01 10:49:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     audioPlayer = new Audio("https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view")
 //     audioPlayer = new Audio("./data/a1.mp3") //  localhost
     audioPlayer.play()
-    document.querySelector(".btn_play").style="display: none"
+    document.querySelector(".btn_play").style="display: block"
     }
 
 // https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
