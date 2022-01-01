752efe7f4e7f65f5c269831db1d26d06c288f3d2
opadam <54212616+opaDam@users.noreply.github.com>
2022-01-01 10:49:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   function my(){
 
 //     audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view")
-    audioPlayer = new Audio(URL="https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view")
+    audioPlayer = new Audio("https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view")
 //     audioPlayer = new Audio("./data/a1.mp3") //  localhost
     audioPlayer.play()
     document.querySelector(".btn_play").style="display: none"
