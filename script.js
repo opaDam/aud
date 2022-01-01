0843c19ec727d5bf1b0ac1f34a790d7c73dd9069
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-01 13:31:04 +0100

    01

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+let song;
+const audioPlayer = new Audio();
 
-  function my(){
+const list = document.querySelector(".list")
+.addEventListener("click", function (e) {
+  e.preventDefault();
+  song = e.target.href;
+  my();
+});
+function my() {
+  audioPlayer.src = song;
+  audioPlayer.play();
+}
 
 //     audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view")
 //     const song = "https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view?usp=sharing"
 //     const song = 'https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S.mp3';
-    song = "./data/a2.mp3"
-    audioPlayer = new Audio(song)
+
 //     audioPlayer = new Audio("./data/a1.mp3") //  localhost
-    audioPlayer.play()
-    document.querySelector(".btn_play").style="display: flex"
-    }
 
 // https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
 // https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view?usp=sharing
 // https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view?usp=sharing
-
-
