a8224350498389eaff55f8aad27b5ec87649ad60
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-01 13:50:40 +0100

    04

--- script.js
+++ script.js
@@ -11,13 +11,3 @@ function my() {
   audioPlayer.src = song;
   audioPlayer.play();
 }
-
-//     audioPlayer = new Audio(URL="https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view")
-//     const song = "https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view?usp=sharing"
-//     const song = 'https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S.mp3';
-
-//     audioPlayer = new Audio("./data/a1.mp3") //  localhost
-
-// https://drive.google.com/file/d/1ZMO0zxODquRPPEh1hBdKRCVMIgpsmi51/view?usp=sharing
-// https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view?usp=sharing
-// https://drive.google.com/file/d/1f_vrRHKCrXVd9q2dx7ZVA07JZEDYBM8S/view?usp=sharing
