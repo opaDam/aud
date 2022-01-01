966c7de7fe48850a9389854e3dee82d6614eaec6
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-01 22:24:21 +0100

    10

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let song;
+let song, text;
 const audioPlayer = new Audio();
 
+const print_song =document.querySelector(".prt_song");
 const list = document.querySelector(".list");
 list.addEventListener("click", function (e) {
   e.preventDefault();
   song = e.target.href;
+  text = e.target.innerText;
+  console.log(e);
   my();
 });
 function my() {
   audioPlayer.src = song;
   audioPlayer.play();
+  print_song.innerText = text
 }
