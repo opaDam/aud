4b7a064b58e836b79b098158126e00d66135b2aa
opadam <54212616+opaDam@users.noreply.github.com>
2022-01-07 00:11:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function my() {
 //   song =  "https://www.pengshu.net/lyrics/content/songs/zhui_meng_chi_zi_xin.mp3"
 //   song = "https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3"
 //   song = "https://archive.org/details/CelineDion_A-New-Day-Has-Come_03222002/01+I'm+Alive.mp3"
-await  song = "https://archive.org/details/super-romantic-collection-vol.1-10/Super+Romantic+Collection+(Vol.1-10)/CD-01/001+Marilyn+Monroe+-+I+wanna+be+loved+by+You.mp3"
+  song = await "https://archive.org/details/super-romantic-collection-vol.1-10/Super+Romantic+Collection+(Vol.1-10)/CD-01/001+Marilyn+Monroe+-+I+wanna+be+loved+by+You.mp3"
   audioPlayer.src = song;
   audioPlayer.play();
   print_song.innerText = text;
